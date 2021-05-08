f617723e945fe88a8e76393a61031c27ba6001d8
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2021-05-08 03:28:20 -0400

    refactored typing component

---
+++ src/components/TypingComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <b>{{ underConstruction }}</b><b class="blinking">|</b>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        text: {
+            type: String,
+            default: ''
+            
+        }
+    },
+    data () {
+        return {
+            underConstruction:'',
+        }
+    },
+    methods: {
+            async underConstructionLoop () {
+      let typingDelay = 100
+      let excitementDelay = typingDelay * 20
+      console.log("here we go")
+      // let excitementDelay = typingDelay * 3
+      if(this.underConstruction == ''){
+        for(let i = 0; i < this.text.length; i++){
+          this.underConstruction += this.text.charAt(i)
+          await new Promise(r => setTimeout(r,typingDelay))
+        }
+        await new Promise(r => setTimeout(r,excitementDelay))
+      } else {
+        for(let i = 0; i < this.text.length; i++){
+          this.underConstruction = this.underConstruction.substr(0,this.underConstruction.length - 1)
+          await new Promise(r => setTimeout(r,typingDelay/2))
+        }
+      }
+      this.underConstructionLoop()
+    }
+  },
+  mounted (){
+    this.underConstructionLoop()
+  }
+})
+</script>
+
+<style scoped>
+</style>
