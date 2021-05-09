f323fb88ef4bc40a0627756d2661f78abf279e4c
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2021-05-08 20:26:12 -0400

    added ubuntu webpack

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,9 @@
       <p>Powered by 
             <i title="vuejs" class="devicon-vuejs-plain colored"></i>
             <i title="TypeScript" class="devicon-typescript-plain colored"></i>
+            <i title="Ubuntu" class="devicon-ubuntu-plain colored"></i>
+            <i title="webpack" class="devicon-webpack-plain colored"></i>
+            <i title="NPM" class="devicon-npm-original-wordmark colored"></i>
             <i title="GitHub" class="devicon-github-original"></i>
       </p>
       <p><a href="https://github.com/kaovilai/kaovilai.pw">See this code on GitHub!</a></p>
