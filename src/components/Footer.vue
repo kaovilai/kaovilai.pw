69c93c242aeddfe0029d7314043738ff20090ed0
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2021-05-08 20:07:02 -0400

    footer reorder

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-      <p><a href="https://github.com/kaovilai/kaovilai.pw">See this code on GitHub!</a></p>
       <p>Powered by 
             <i title="vuejs" class="devicon-vuejs-plain colored"></i>
             <i title="TypeScript" class="devicon-typescript-plain colored"></i>
             <i title="GitHub" class="devicon-github-original"></i>
       </p>
+      <p><a href="https://github.com/kaovilai/kaovilai.pw">See this code on GitHub!</a></p>
 </template>
 
 <script>
